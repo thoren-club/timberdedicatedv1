fd221ddbbe84eaf94becf9cf1bdf4b5753bc2afe
gleb <lunnyweal@gmail.com>
2026-07-18 13:20:27 +0700

    Keep only Linux server archive

--- Start Dedicated Server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-exec ./TimberShiftDedicatedServer.x86_64 \
-  --headless \
-  --audio-driver Dummy \
-  --log-file "$(pwd)/server.log" \
-  -- \
-  --server
